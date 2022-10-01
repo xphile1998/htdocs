8051231a652b80cceb984c89ce98e04eb485abf9
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-01 11:56:59 -0700

    Getting ready to do my video

--- phpmotors/library/connections.php
+++ phpmotors/library/connections.php
@@ -13,13 +13,15 @@
 
     try {
         $link = new PDO($dsn, $username, $password, $options);
-        // if(is_object($link)) {
-        //     echo 'It worked!';
-        // }
+        if(is_object($link)) {
+            echo 'It worked!';
+        }
         return $link;
     } catch(PDOException $e) {
         // echo "It didn't work, error: " . $e->getMessage();
-        header('Location: /phpmotors/view/server_error.php');
+        header('Location: /phpmotors/view/500.php');
         exit;
     }
  }
+
+ phpmotorsConnect();
