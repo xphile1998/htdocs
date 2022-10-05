262830adaa4538559f9d45834b75dd02b5812a1c
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-05 16:44:16 -0700

    Finishing the index.php file.

---
+++ phpmotors/index.php
@@ -0,0 +1,16 @@
+<?php
+$action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL) {
+        $action = filter_input(INPUT_GET, 'action');
+    }
+
+switch ($action) {
+    case 'something': 
+
+        break;
+
+    default:
+        include 'view/home.php';
+}
+
+?>
