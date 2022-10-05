69ea7afc00bd42ca7f5aaf50d3abdae9bc05f356
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-05 16:27:04 -0700

    Finished the lesson on variables.

--- sandbox/week4.php
+++ sandbox/week4.php
@@ -1,4 +1,15 @@
 <?php
-$username = "HeMan";
+$fullname = 'Eron Whitefield';
+$displayname = 'Your name is $fullname';
+$displayname2 = "Your name is $fullname";
+$displayname3 = 'Your name is '.$fullname.'.';
 
-echo "The username is " . $username;
+echo $displayname;
+echo "</br></br>";
+
+echo $displayname2;
+echo '</br></br>';
+
+echo $displayname3;
+echo "</br></br>";
+
