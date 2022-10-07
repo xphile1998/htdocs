8bfce658634334e407cbe1b0fa943a621615acdb
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-06 23:52:11 -0700

    playing more

--- phpmotors/model/main-model.php
+++ phpmotors/model/main-model.php
@@ -12,7 +12,7 @@ function getClassifications() {
     $stmt->execute();
     // The next line gets the data from the database and
     // stores it as an array in the $classifications variable
-    $classifications = $stmt->fetchAll();
+    $classifications = $stmt->fetchAll(PDO::FETCH_ASSOC);
     // the next line closes the interaction with the database
     $stmt->closeCursor();
     // The next line send the array of data back to thwhere the function
