d8166909477b0b2a943b9f9acd1657ca0bcaddf6
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-14 11:58:33 -0700

    Making style changes to my code to make it more readable to me.

--- phpmotors/index.php
+++ phpmotors/index.php
@@ -1,17 +1,20 @@
 <?php
-// This is the main controller for the site
+/*==============================\\
+||                              ||  
+||  MAIN CONTROLLER             ||
+||                              ||
+\\==============================*/
 
-// Get the database connection file
+/*======================================================\\
+||  We are going to get the following support files:    ||
+||    + Database connection file                        ||
+||    + Main Model file                                 ||
+\\======================================================*/
 require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/connections.php';
-
-// Get the PHP Motors Model for use as needed
 require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/model/main-model.php';
 
 // Get the array of classifications
 $classifications = getClassifications();
-// var_dump($classifications);
-// echo '<pre>'.print_r($classifications, true).'</pre>';
-// exit;
 
 // Build a navigation bar using the $classifications arrary
 $navList = '<ul>';
@@ -20,8 +23,6 @@ foreach ($classifications as $classification) {
     $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
 }
 $navList .= '</ul>';
-// echo $navList;
-// exit;
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
