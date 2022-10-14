627f8616946c11cf4cdc3826c3c6f4f8ca497a59
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-14 11:54:40 -0700

    Working through Sis Woods Week 5 intro video

---
+++ phpmotors/accounts/index copy.php
@@ -0,0 +1,42 @@
+<?php
+// This is the accounts controller for the site
+
+// Get the database connection file
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/connections.php';
+
+// Get the PHP Motors Model for use as needed
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/model/main-model.php';
+
+// Get the array of classifications
+$classifications = getClassifications();
+// var_dump($classifications);
+// exit;
+
+// Build a navigation bar using the $classifications arrary
+$navList = '<ul>';
+$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
+foreach ($classifications as $classification) {
+    $navList .= "<li><a href='/phpmotors/index.php?action=".urlencode($classification['classificationName'])."' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+}
+$navList .= '</ul>';
+// echo $navList;
+// exit;
+
+$action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL) {
+        $action = filter_input(INPUT_GET, 'action');
+    }
+
+switch ($action) {
+    case 'deliverLoginView':
+        include $_SERVER['DOCUMENT_ROOT'] .'/phpmotors/view/login.php';
+        break;
+
+    case 'deliverRegisterView': 
+        include $_SERVER['DOCUMENT_ROOT'] .'/phpmotors/view/registration.php';
+        break;
+
+    default:
+        // echo 'There is currently nothing for this to go to as a default. Sorry!';
+        break;
+}
--- phpmotors/accounts/index.php
+++ phpmotors/accounts/index.php
@@ -1,39 +1,51 @@
 <?php
-// This is the accounts controller for the site
+/*==============================\\
+||                              ||  
+||  ACCOUNTS CONTROLLER         ||
+||                              ||
+\\==============================*/
 
-// Get the database connection file
+// Create or access a session
+session_start();
+
+/*======================================================\\
+||  We are going to get the following support files:    ||
+||    + Database connection file                        ||
+||    + Main Model file                                 ||
+||    + Accounts Model file                             ||
+||    + Functions file                                  ||
+\\======================================================*/
 require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/connections.php';
-
-// Get the PHP Motors Model for use as needed
 require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/model/main-model.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/model/accounts_model.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/functions.php';
 
 // Get the array of classifications
 $classifications = getClassifications();
-// var_dump($classifications);
-// exit;
+
+// Default Page Title
+$pageTitle = 'Accounts';
 
 // Build a navigation bar using the $classifications arrary
 $navList = '<ul>';
 $navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
 foreach ($classifications as $classification) {
-    $navList .= "<li><a href='/phpmotors/index.php?action=".urlencode($classification['classificationName'])."' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+    $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
 }
 $navList .= '</ul>';
-// echo $navList;
-// exit;
 
 $action = filter_input(INPUT_POST, 'action');
-    if ($action == NULL) {
-        $action = filter_input(INPUT_GET, 'action');
-    }
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+}
 
 switch ($action) {
     case 'deliverLoginView':
-        include $_SERVER['DOCUMENT_ROOT'] .'/phpmotors/view/login.php';
+        include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/view/login.php';
         break;
 
-    case 'deliverRegisterView': 
-        include $_SERVER['DOCUMENT_ROOT'] .'/phpmotors/view/registration.php';
+    case 'deliverRegisterView':
+        include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/view/registration.php';
         break;
 
     default:
