4eab40e5b152426dc0baeecd3cd67aaaae97a712
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-15 13:44:35 -0700

    Created this to follow along.

---
+++ troubleshooting.php
@@ -0,0 +1,28 @@
+<?php
+/*==============================\\
+||                              ||
+||  WEEK 5 - TROUBLESHOOTING    ||
+||  TECHNIQUES                  ||
+||                              ||
+\\==============================*/
+
+// Variables used
+$variable = "I am a string";    // Obviously a string
+$var1 = 5;                      // I am an integer
+$var2 = 7.4;                    // I am a double
+$var3 = "100.5";                // Another string (see the quotes)
+$sampleArray = ['Chevy', 'Ford', 'Mazda', 'Subaru'];
+
+// Echo statements
+// echo $variable.'<br>';
+// echo "$var1, $var2, $var3".'<br>';
+// echo '$var1, $var2, $var3';
+
+// Var dumps
+// var_dump($variable);
+// var_dump($sampleArray);
+
+// print_r
+// print_r($sampleArray);
+
+exit;
