85a590cf72e375f07107ba38323b044d71ed0a1e
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-15 16:10:18 -0700

    Getting set up for Enhancement 4

---
+++ phpmotors/vehicles/index.php
@@ -0,0 +1,75 @@
+<?php
+/*==============================\\
+||                              ||  
+||  VEHILCES CONTROLLER         ||
+||                              ||
+\\==============================*/
+
+/*======================================================\\
+||  We are going to get the following support files:    ||
+||    + Database connection file                        ||
+||    + Main Model file                                 ||
+||    + Vehicles Model file                             ||
+\\======================================================*/
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/connections.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/model/main-model.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/model/vehicles-model.php';
+
+// Get the array of classifications
+$classifications = getClassifications();
+
+// Default Page Title
+$pageTitle = 'Accounts';
+
+// Build a navigation bar using the $classifications arrary
+$navList = '<ul>';
+$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
+foreach ($classifications as $classification) {
+    $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+}
+$navList .= '</ul>';
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+}
+
+switch ($action) {
+    case '':
+        
+        break;
+
+    case 'registerUser':
+        // Collect, filter, and store the user data
+        $clientFirstname = filter_input(INPUT_POST, 'clientFirstname');
+        $clientLastname = filter_input(INPUT_POST, 'clientLastname');
+        $clientEmail = filter_input(INPUT_POST, 'clientEmail');
+        $clientPassword = filter_input(INPUT_POST, 'clientPassword');
+
+        // Check to see if there is any missing data
+        if (empty($clientFirstname) || empty($clientLastname) || empty($clientEmail) || empty($clientPassword)) {
+            $message = '<p class="message">Please provide all the information for all empty fields.</p>';
+            include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/view/registration.php';
+            exit;
+        }
+
+        // Send the data to the database
+        $regOutcome = regClient($clientFirstname, $clientLastname, $clientEmail, $clientPassword);
+
+        // Check out the result of the INSERT into the database
+        if ($regOutcome === 1) {
+            $message = '<p class="message">Thanks for registering $clientFirstname. Please use your email and password to login.</p>';
+            include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/view/login.php';
+            exit;
+        } else {
+            $message = '<p class="message">Sorry, $clientFirstname, but the registration failed. Please try again.</p>';
+            include $_SERVER['DOCUMNET_ROOT'] . '/phpmotors/view/registration.php';
+            exit; 
+        }
+        
+        break;
+
+    default:
+        // echo 'There is currently nothing for this to go to as a default. Sorry!';
+        break;
+}
---
+++ phpmotors/view/add-vehicle.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+    <link rel = "stylesheet" media="screen" href = "/phpmotors/css/normalize.css">
+    <link rel = "stylesheet" media="screen" href = "/phpmotors/css/main.css">
+    <link rel = "stylesheet" media="screen" href = "/phpmotors/css/medium.css">
+</head>
+
+<body>
+<div id="content_box">
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+    </header>
+
+    <nav class="navigation">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/nav.php'; ?>
+    </nav>
+
+    <main>
+        <h1>Content Title Here</h1>
+    </main>
+    
+    <hr>
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
+    </footer>
+
+</div>
+</body>
+</html>
