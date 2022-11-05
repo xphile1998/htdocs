072166f00ef60f468031599681e0c838faf3db77
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-11-05 10:36:06 -0700

    Need to update VSC

--- phpmotors/snippets/header.php
+++ phpmotors/snippets/header.php
@@ -1,7 +1,9 @@
 <img src="/phpmotors/images/site/logo.png" alt="PHP Motors Logo">
-<?php
-if (isset($cookieFirstName)) {
-    echo "<span>Welcome $cookieFirstName</span>";
-}
-?>
-<a class="head_para" href="accounts/?action=deliverLoginView" title="Login or Register with PHP Motors">My Account</a>
+<div class="head_para">
+    <?php
+    if (isset($cookieFirstname)) {
+        echo "<span>Welcome $cookieFirstname</span>";
+    }
+    ?>
+    <a href="accounts/?action=deliverLoginView" title="Login or Register with PHP Motors">My Account</a>
+</div>
