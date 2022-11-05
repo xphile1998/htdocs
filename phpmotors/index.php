b7bcc0d8f2e00a9e27a15df9f2a9fe17616bd61c
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-11-04 19:33:18 -0700

    Updating...

--- phpmotors/index.php
+++ phpmotors/index.php
@@ -20,14 +20,14 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/functions.php';
 $classifications = getClassifications();
 $navList = buildNavList($classifications);
 
-$action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-}
-
 // Check if the firstname cookie exists, get its value
 if (isset($_COOKIE['firstname'])) {
     $cookieFirstname = filter_input(INPUT_COOKIE, 'firstname', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+}
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
 }
 
 switch ($action) {
