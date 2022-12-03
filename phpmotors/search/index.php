a613f0c7a8280b28cc7217db3468ac9c65fb5ac5
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-12-03 10:24:00 -0800

    Setting the stage for the final project

---
+++ phpmotors/search/index.php
@@ -0,0 +1,22 @@
+<?php
+/*==============================\\
+||                              ||  
+||  SEARCH CONTROLLER           ||
+||                              ||
+\\==============================*/
+
+// Create or access a session
+session_start();
+
+/*======================================================\\
+||  We are going to get the following support files:    ||
+||    + Database connection file                        ||
+||    + Main Model file                                 ||
+||    + Vehicles Model file                             ||
+||    + Functions file
+\\======================================================*/
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/connections.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/model/main-model.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/model/vehicles-model.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/functions.php';
+
